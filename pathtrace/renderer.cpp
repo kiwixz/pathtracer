d23af6c5406264318eedff28b26028f6180d329b
kiwixz <kiwixz@outlook.com>
2018-07-28 14:35:22 +0200

    randomize sample distribution

--- pathtrace/renderer.cpp
+++ pathtrace/renderer.cpp
@@ -46,12 +46,13 @@ namespace pathtracer {
             switch (shape.material.reflection) {
             case Material::Reflection::diffuse:  // TODO WIP
                 double r1 = random(2 * pi), r2 = random(), r2s = std::sqrt(r2);
-                glm::dvec3 u = glm::normalize(glm::cross(
-                        std::abs(oriented_normal.x) > .1 ? glm::dvec3(0, 1, 0) : glm::dvec3(1, 0, 0),
-                        oriented_normal));
+                glm::dvec3 u = glm::normalize(
+                        glm::cross(std::abs(oriented_normal.x) > .1 ? glm::dvec3(0, 1, 0)
+                                                                    : glm::dvec3(1, 0, 0),
+                                oriented_normal));
                 glm::dvec3 v = glm::cross(oriented_normal, u);
                 glm::dvec3 d = glm::normalize(u * std::cos(r1) * r2s + v * std::sin(r1) * r2s
-                                             + oriented_normal * std::sqrt(1 - r2));
+                                              + oriented_normal * std::sqrt(1 - r2));
                 return shape.material.emission
                        + shape.material.color * radiance({intersection, d}, scene, depth + 1);
             }
@@ -68,20 +69,21 @@ namespace pathtracer {
         projection[1][3] = scene.camera.position.y;
         projection[2][3] = scene.camera.position.z;
 
+        double x_delta = 1.0 / scene.settings.width;
+        double y_delta = 1.0 / scene.settings.height;
         std::vector<Color> pixels(scene.settings.width * scene.settings.height);
         for (int y = 0; y < scene.settings.height; ++y) {
             for (int x = 0; x < scene.settings.width; ++x) {
                 Color& color = pixels[y * scene.settings.width + x];
                 color = {0, 0, 0};
+                glm::dvec3 pixel_on_screen{
+                        (x * x_delta - .5) * scene.settings.width / scene.settings.height,
+                        .5 - y * y_delta, scene.camera.focal};
                 for (int i = 0; i < scene.settings.samples; ++i) {
-                    glm::dvec3 point_on_screen{(x / static_cast<double>(scene.settings.width) - .5)
-                                                      * scene.settings.width
-                                                      / scene.settings.height,
-                            .5 - y / static_cast<double>(scene.settings.height),
-                            scene.camera.focal};
-                    glm::dvec3 eye_to_screen = projection * glm::dvec4{point_on_screen, 1};
+                    glm::dvec4 point_on_screen{pixel_on_screen.x + random(x_delta),
+                            pixel_on_screen.y + random(y_delta), pixel_on_screen.z, 1};
+                    glm::dvec3 eye_to_screen = projection * point_on_screen;
                     Ray ray{scene.camera.position, glm::normalize(eye_to_screen)};
-
                     color += radiance(ray, scene) / static_cast<float>(scene.settings.samples);
                 }
             }
