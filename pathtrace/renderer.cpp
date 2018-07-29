45a658c5784071d58566d6d0fa4d9d4122d47876
kiwixz <kiwixz@outlook.com>
2018-07-29 04:53:37 +0200

    fast rand, intersection epsilon, cleanup diffuse

--- pathtrace/renderer.cpp
+++ pathtrace/renderer.cpp
@@ -7,57 +7,6 @@
 namespace pathtracer {
     namespace {
         constexpr Color background_color{0, 0, 0};
-
-        double random(double a, double b)
-        {
-            double rnd = rand() / static_cast<double>(RAND_MAX);
-            return rnd * (b - a) + a;
-        }
-        double random(double b = 1)
-        {
-            return random(0, b);
-        }
-
-        Color radiance(const Ray& ray, const Scene& scene, int depth = 0)
-        {
-            const Shape* shape_ptr = nullptr;
-            double distance = std::numeric_limits<double>::infinity();
-            for (const std::unique_ptr<Shape>& s : scene.shapes) {
-                std::optional<double> new_distance = s->intersect(ray);
-                if (new_distance && new_distance < distance) {
-                    distance = new_distance.value();
-                    shape_ptr = s.get();
-                }
-            }
-
-            if (!shape_ptr)
-                return background_color;
-
-            const Shape& shape = *shape_ptr;
-
-            if (depth > 2)  // TODO russian roulette with color
-                return shape.material.emission;
-
-            glm::dvec3 intersection = ray.origin + ray.direction * distance;
-            glm::dvec3 normal = shape.normal(intersection);
-            glm::dvec3 oriented_normal = glm::dot(normal, ray.direction) > 0 ? -normal : normal;
-
-            switch (shape.material.reflection) {
-            case Material::Reflection::diffuse:  // TODO WIP
-                double r1 = random(glm::two_pi<double>()), r2 = random(), r2s = std::sqrt(r2);
-                glm::dvec3 u = glm::normalize(
-                        glm::cross(std::abs(oriented_normal.x) > .1 ? glm::dvec3(0, 1, 0)
-                                                                    : glm::dvec3(1, 0, 0),
-                                oriented_normal));
-                glm::dvec3 v = glm::cross(oriented_normal, u);
-                glm::dvec3 d = glm::normalize(u * std::cos(r1) * r2s + v * std::sin(r1) * r2s
-                                              + oriented_normal * std::sqrt(1 - r2));
-                return shape.material.emission
-                       + shape.material.color * radiance({intersection, d}, scene, depth + 1);
-            }
-
-            return {0, 1, 0};
-        }
     }  // namespace
 
     Image Renderer::render(const Scene& scene)
@@ -79,8 +28,8 @@ namespace pathtracer {
                         (x * x_delta - .5) * scene.settings.width / scene.settings.height,
                         .5 - y * y_delta, scene.camera.focal};
                 for (int i = 0; i < scene.settings.samples; ++i) {
-                    glm::dvec4 point_on_screen{pixel_on_screen.x + random(x_delta),
-                            pixel_on_screen.y + random(y_delta), pixel_on_screen.z, 1};
+                    glm::dvec4 point_on_screen{pixel_on_screen.x + gen_(x_delta),
+                            pixel_on_screen.y + gen_(y_delta), pixel_on_screen.z, 1};
                     glm::dvec3 eye_to_screen = projection * point_on_screen;
                     Ray ray{scene.camera.position, glm::normalize(eye_to_screen)};
                     color += radiance(ray, scene) / static_cast<float>(scene.settings.samples);
@@ -88,5 +37,51 @@ namespace pathtracer {
             }
         }
         return {std::move(pixels), scene.settings.width, scene.settings.height};
+    }
+
+    Color Renderer::radiance(const Ray& ray, const Scene& scene, int depth)
+    {
+        const Shape* shape_ptr = nullptr;
+        double distance = std::numeric_limits<double>::infinity();
+        for (const std::unique_ptr<Shape>& s : scene.shapes) {
+            std::optional<double> new_distance = s->intersect(ray);
+            if (new_distance && new_distance < distance) {
+                distance = new_distance.value();
+                shape_ptr = s.get();
+            }
+        }
+
+        if (!shape_ptr)
+            return background_color;
+
+        const Shape& shape = *shape_ptr;
+
+        if (depth > 2)  // TODO russian roulette with color
+            return shape.material.emission;
+
+        glm::dvec3 intersection = ray.origin + ray.direction * (distance - 1e-9);  // TODO justify epsilon
+        glm::dvec3 normal = shape.normal(intersection);
+        glm::dvec3 oriented_normal = glm::dot(normal, ray.direction) > 0 ? -normal : normal;
+
+        switch (shape.material.reflection) {
+        case Material::Reflection::diffuse:
+            double angle = gen_(glm::two_pi<double>());
+            double normal_deviation = gen_();
+            double normal_deviation_sq = std::sqrt(normal_deviation);
+            glm::dvec3 u = glm::normalize(glm::cross(
+                    std::abs(oriented_normal.x) < std::abs(oriented_normal.y) ? glm::dvec3{1, 0, 0}
+                                                                              : glm::dvec3{0, 1, 0},
+                    oriented_normal));
+            glm::dvec3 v = glm::cross(oriented_normal, u);
+            glm::dvec3 direction =
+                    glm::normalize(u * std::cos(angle) * normal_deviation_sq
+                                   + v * std::sin(angle) * normal_deviation_sq
+                                   + oriented_normal * std::sqrt(1 - normal_deviation));
+
+            return shape.material.emission
+                   + shape.material.color * radiance({intersection, direction}, scene, depth + 1);
+        }
+
+        return {0, 1, 0};
     }
 }  // namespace pathtracer
--- pathtrace/renderer.h
+++ pathtrace/renderer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "pathtrace/fast_rand.h"
 #include "pathtrace/image.h"
 #include "pathtrace/scene.h"
 
@@ -7,5 +8,10 @@ namespace pathtracer {
     class Renderer {
     public:
         Image render(const Scene& scene);
+
+    private:
+        FastRand gen_;
+
+        Color radiance(const Ray& ray, const Scene& scene, int depth = 0);
     };
 }  // namespace pathtracer
