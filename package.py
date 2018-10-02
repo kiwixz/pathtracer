df6cb183ac669a56e87d681a675ec5fc917fd1fa
kiwixz <kiwixz@outlook.com>
2018-10-02 20:27:37 +0100

    copy required dll instead of full redist

--- package.py
+++ package.py
@@ -3,6 +3,7 @@
 import json
 import logging
 import os
+import re
 import shutil
 import subprocess
 import tempfile
@@ -15,24 +16,28 @@ OUTPUT_FILE = f"build/{APP_NAME}.zip"
 VSWHERE_VERSION = "2.5.2"
 
 
+# actions
+
+
 def cd(path):
     logging.debug(f"change directory to '{os.path.realpath(path)}'")
     os.chdir(path)
 
 
-def copy(src, dest, pattern="**/*"):
-    if os.path.isfile(src):
-        logging.debug(f"copy '{src}' to '{dest}'")
-        shutil.copy2(src, dest)
-    else:
-        logging.debug(f"copy '{pattern}' from '{src}' to '{dest}'")
-        os.makedirs(dest, exist_ok=True)
-        for src_f in src.glob(pattern):
-            dest_f = dest / src_f.relative_to(src)
-            if os.path.isdir(src_f):
-                os.makedirs(dest_f, exist_ok=True)
-            else:
-                shutil.copy2(src_f, dest_f)
+def copy(src, dest):
+    logging.debug(f"copy '{src}' to '{dest}'")
+    shutil.copy2(src, dest)
+
+
+def copy_all(src, dest, pattern="**/*"):
+    logging.debug(f"copy '{pattern}' from '{src}' to '{dest}'")
+    os.makedirs(dest, exist_ok=True)
+    for src_f in src.glob(pattern):
+        dest_f = dest / src_f.relative_to(src)
+        if os.path.isdir(src_f):
+            os.makedirs(dest_f, exist_ok=True)
+        else:
+            shutil.copy2(src_f, dest_f)
 
 
 def run(cmd):
@@ -45,6 +50,19 @@ def run_output(cmd):
     return subprocess.check_output(cmd, shell=True)
 
 
+# helpers
+
+
+def last_version(path):
+    versions = os.listdir(path)
+    versions.sort(key=lambda version: [int(n) if n.isdigit() else 0
+                                       for n in version.split(".")])
+    return path / versions[-1]
+
+
+# steps
+
+
 def find_vs_path():
     logging.info("search VS path")
     vswhere_exe = Path(os.environ["ProgramFiles(x86)"]) / "Microsoft Visual Studio" / "Installer" / "vswhere.exe"
@@ -54,28 +72,39 @@ def find_vs_path():
     return vs_path
 
 
+def find_required_dlls(package_dir):
+    logging.info("search required DLLs")
+    tools_path = Path(vs_path) / "VC" / "Tools" / "MSVC"
+    dumpbin_exe = last_version(tools_path) / "bin" / "Hostx64" / "x64" / "dumpbin.exe"
+    dlls_list = set()
+    for exe in package_dir.glob("**/*.exe"):
+        raw = run_output(rf'"{dumpbin_exe}" /DEPENDENTS {exe}')
+        matches = re.findall(r'(^|\s)([\w.-]+\.dll)($|\s)', raw.decode("utf8"))
+        dlls_list = dlls_list.union([match[1] for match in matches])
+    return dlls_list
+
+
 def copy_redist(vs_path, package_dir):
+    required_dlls = [dll.lower() for dll in find_required_dlls(package_dir)]
     logging.info("copy redist")
     redist_path = Path(vs_path) / "VC" / "Redist" / "MSVC"
-    msvc_versions = os.listdir(redist_path)
-    msvc_versions.sort(key=lambda version: [int(n) for n in version.split(".")])
-    copy(redist_path / msvc_versions[-1] / "vc_redist.x64.exe", package_dir)
+    redist_path = last_version(redist_path) / "x64"
+    for dll in redist_path.glob("**/*"):
+        if dll.name.lower() in required_dlls:
+            copy(dll, package_dir)
 
 
 def build_app(vs_path):
     logging.info("build solution")
     msbuild_path = Path(vs_path) / "MSBuild"
-    msbuild_versions = os.listdir(msbuild_path)
-    msbuild_versions.sort(key=lambda version: [int(n) if n.isdigit() else 0
-                                               for n in version.split(".")])
-    msbuild_exe = msbuild_path / msbuild_versions[-1] / "Bin" / "MSBuild"
+    msbuild_exe = last_version(msbuild_path) / "Bin" / "MSBuild.exe"
     run(rf'"{msbuild_exe}" /m /v:m /p:Configuration=Release /p:Platform=x64')
 
 
 def copy_app(package_dir):
     logging.info(f"copy {APP_NAME}")
-    copy(Path("build") / "x64-Release", package_dir, "*.dll")
-    copy(Path("build") / "x64-Release", package_dir, "*.exe")
+    copy_all(Path("build") / "x64-Release", package_dir, "*.dll")
+    copy_all(Path("build") / "x64-Release", package_dir, "*.exe")
 
 
 def zip_app(package_dir):
@@ -84,6 +113,9 @@ def zip_app(package_dir):
         for f in package_dir.glob("**/*"):
             if os.path.isfile(f):
                 zipf.write(f, f"{APP_NAME}/{f.relative_to(package_dir)}")
+
+
+# main
 
 
 if __name__ == "__main__":
@@ -95,9 +127,9 @@ if __name__ == "__main__":
     package_dir = Path(tempfile.mkdtemp())
     try:
         vs_path = find_vs_path()
-        copy_redist(vs_path, package_dir)
         build_app(vs_path)
         copy_app(package_dir)
+        copy_redist(vs_path, package_dir)
         zip_app(package_dir)
     finally:
         shutil.rmtree(package_dir)
